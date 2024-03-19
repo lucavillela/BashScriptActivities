f2024f023605570350af512a1d015b124db837f9
Luca Villela <lucatvillela@gmail.com>
2024-03-19 20:40:16 -0300

    implemented -v verbose

--- FPS-ATV02/script1.sh
+++ FPS-ATV02/script1.sh
@@ -4,48 +4,45 @@ date=$(date '+%Y-%m-%d_%H:%M:%S') #armazena a data atual
 
 function backup(){
 	if [ -d "Backup" ]; then
-    		rm -r ./Backup #apaga backup caso já exista
+    		rm -v -r ./Backup #apaga backup caso já exista
 	fi
 	
-	mkdir Backup #cria uma nova pasta backup
+	mkdir -v Backup #cria uma nova pasta backup
 	for arq in *
 	do 
 		if [ "$arq" != "Backup" ] #para cada arquivo que não é a pasta backup
 		then
-			cp -r $arq "./Backup/${arq}_$date" #copie o arquivo para pasta Backup com seu nome acrescido da data atual
-			echo "$arq copiado para diretório ./Backup como: ${arq}_$date" #printe o que foi feito
+			cp -v -r $arq "./Backup/${arq}_$date" #copie o arquivo para pasta Backup com seu nome acrescido da data atual
 		fi
 	done
 }
 
 function temp1(){
 	if [ -d "Tmp" ]; then
-    		rm -r ./Tmp #caso Tmp já exista ele apaga
+    		rm -v -r ./Tmp #caso Tmp já exista ele apaga
 	fi
 	
-	mkdir Tmp #cria uma nova pasta Tmp
+	mkdir -v Tmp #cria uma nova pasta Tmp
 	for arq in *
 	do 
 		if [ "$arq" != "Tmp" ] && [ "$arq" = [a,e,i,o,u,A,E,I,O,U]* ] #para cada arquivo que não seja pasta Tmp e comece com vogal
 		then
-			cp -r $arq ./Tmp #copie o arquivo para pasta Tmp
-			echo "$arq copiado para diretório ./Tmp" #printe o que foi feito
+			cp -v -r $arq ./Tmp #copie o arquivo para pasta Tmp
 		fi
 	done
 }
 
 function temp2(){
 	if [ -d "Tmp" ]; then
-    		rm -r ./Tmp #caso Tmp já exista ele apaga
+    		rm -v -r ./Tmp #caso Tmp já exista ele apaga
 	fi
 	
-	mkdir Tmp #cria uma nova pasta Tmp
+	mkdir -v Tmp #cria uma nova pasta Tmp
 	for arq in *
 	do 
 		if [ "$arq" != "Tmp" ] && [ "$arq" != [a,e,i,o,u,A,E,I,O,U]* ] #para cada arquivo que não seja pasta Tmp e comece com consoante 
 		then
-			cp -r $arq ./Tmp copie o arquivo para pasta Tmp
-			echo "$arq copiado para diretório ./Tmp" #printe o que foi feito
+			cp -v -r $arq ./Tmp copie o arquivo para pasta Tmp
 		fi
 	done
 }
@@ -73,13 +70,11 @@ case $arg in
 		temp2 ;;
 	*) #caso argumento não seja um dos casos esperados
 		if [ -d "Tmp" ]; then
-    			rm -r ./Tmp #remova a pasta Tmp
-    			echo "Diretório ./Tmp removido" 
+    			rm -v -r ./Tmp #remova a pasta Tmp
 		fi
 		
 		if [ -d "Backup" ]; then
-    			rm -r ./Backup #remova a pasta Backup
-			echo "Diretório ./Backup removido"
+    			rm -v -r ./Backup #remova a pasta Backup
 		fi ;;
 esac
 		
